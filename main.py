8b7a9818e1ddf148f331e1de6175a758e012dc4c
Jadedilok <jadedilok909@gmail.com>
2024-08-31 10:01:37 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from app1401 import testIf
+from app1402 import testElse
+from app1403 import testGrade
+from app1404 import testNestedIf
+from app1405 import testTenary
+from app1406 import testForLoop
+from app1407 import testRange
+
+# testIf()  # 1.If statement
+
+# testElse()  # 2.If Else statementgit remote add origin https://github.com/Jadedilok/python-week14-1567.git
+
+
+# testGrade()  # 3.If elif else statement
+
+# testNestedIf()  # 4.The nested if statement
+
+# testTenary()  #5.Python Ternary Operator
+
+# testForLoop()  #6.ForLoop
+
+# testRange(6)  #7.Use of range()
